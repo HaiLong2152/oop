990af49e6607728ce3d5763f3ecf461c07511937
HaiLong2152 <daomanhhailong2006@gmail.com>
2025-09-30 23:22:43 +0700

    HW1

---
+++ HW1/MaxMin.java
@@ -0,0 +1,18 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class MaxMin {
+    public static void main(String[] args) {
+
+        int max = StdIn.readInt();
+        int min = max;
+
+        while (!StdIn.isEmpty()) {
+            int value = StdIn.readInt();
+            if (value > max) max = value;
+            if (value < min) min = value;
+        }
+
+        StdOut.println("maximum  = " + max + ", minimum = " + min);
+    }
+}
