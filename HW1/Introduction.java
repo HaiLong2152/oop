df0a5f3ae497c7cdeff47bb3a98478bda894963f
HaiLong2152 <daomanhhailong2006@gmail.com>
2025-09-30 23:08:31 +0700

    intro

---
+++ HW1/Introduction.java
@@ -0,0 +1,10 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Đào Mạnh Hải Long\t24020205\tIT6\tHaiLong2152\tdaomanhhailong2006@gmail.com");
+        for (int i = 9; i >= 1; --i) {
+            System.out.println(i + " bottles of beer on the wall, " + i +" bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
